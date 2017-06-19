f38c7b46a3b1a092d5458cbcd6f90317d5756f0f
Xiaomu0 <w1724323603@163.com>
2017-06-19 12:48:10 +0800

    功能改进：不再约束 practice 下源文件的文件名 可以为每个题目创建多个源代码实例，并可使用 test.sh 自动测试所有实例

--- practice/test.sh
+++ practice/test.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
+dir_name=$(basename $(pwd))
+
 make
 
-for f in *.cc
+for f in *.cc *.c
 do
-    echo "now doing $f"
-    exe=$(echo $f | cut -f1 -d.)
-    ./$exe < ../sample/input-$exe.txt > $exe.txt
-    diff -u ../sample/output-$exe.txt $exe.txt
     echo
+
+    if [ $f = "*.cc" ] || [ $f = "*.c" ]; then
+        break
+    fi
+
+    echo "now testing $f ..."
+
+    exe=$(echo $f | cut -f1 -d.)
+    my_file="$exe.txt"
+    sample_input_file="../../sample/input-$dir_name.txt"
+    sample_output_file="../../sample/output-$dir_name.txt"
+
+    ./$exe < $sample_input_file > $my_file
+
+    if $(diff -q $sample_output_file $my_file > /dev/null);
+    then
+        echo "$f is the right one :)"
+    else
+        echo "$f is not right :("
+        diff -u --color=always $sample_output_file $my_file
+    fi
 done
